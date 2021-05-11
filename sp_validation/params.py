cac42223258aa026cd814ddfb244db450253bc8b
Emma Aycoberry <emmaaycoberry@emmaay.local>
2021-05-11 09:16:24 +0200

    few changment for local calib

--- sp_validation/params.py
+++ sp_validation/params.py
@@ -28,13 +28,13 @@ verbose = True
 SP_BASE = '{}/shapepipe'.format(os.environ['HOME'])
 
 ## Input tile IDs
-path_tile_ID = '{}/aux/CFIS/tiles_202007/tiles_W3.txt'.format(SP_BASE)
+path_tile_ID = '{}/aux/CFIS/tiles_202007/tiles_P3.txt'.format(SP_BASE)
 
 ## Output file for missing tile ID, can be used as input for re-run
 path_missing_ID = './missing_ID.txt'
 
 ## Weak-lensing galaxy catalog name
-galaxy_cat_path = './final_cat.npy'
+galaxy_cat_path = './P3/final_cat.npy'
 
 ## Output plot directory and subdirs
 plot_dir = './plots/'
