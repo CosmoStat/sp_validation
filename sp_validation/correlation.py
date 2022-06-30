3ffeeeba63ec61b7d56fb4fa7c4b65ea68bfda6a
Martin Kilbinger <martin.kilbinger@cea.fr>
2022-06-30 15:37:58 +0200

    output center coordinates to stats file

--- sp_validation/correlation.py
+++ sp_validation/correlation.py
@@ -636,6 +636,9 @@ def xi_star_gal_tc(
 
     Cross-correlation between galaxy and star ellipticities.
     """
+
+    unit = 'degrees'
+
     cat_gal = treecorr.Catalog(
         ra=ra_gal,
         dec=dec_gal,
@@ -656,8 +659,8 @@ def xi_star_gal_tc(
     )
 
     TreeCorrConfig = {
-        'ra_units': 'degrees',
-        'dec_units': 'degrees',
+        'ra_units': unit,
+        'dec_units': unit,
         'sep_units': 'arcminutes',
         'min_sep': theta_min_amin,
         'max_sep': theta_max_amin,
