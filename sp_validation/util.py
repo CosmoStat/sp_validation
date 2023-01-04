72254cbfddd4c55938e1b76953c5088e284eeea3
Martin Kilbinger <martin.kilbinger@cea.fr>
2023-01-04 11:51:10 +0100

    outsoured vos to cs_util.canfar

--- sp_validation/util.py
+++ sp_validation/util.py
@@ -25,7 +25,7 @@ else:
 def millify(n):
     """Millify.
 
-    Return human-readible names of large numbers
+    Return human-readible names of large numbers.
 
     Parameters
     ----------
@@ -55,7 +55,7 @@ def millify(n):
 def equi_num_bins(values, n_bin):
     """Equi Num Bins.
 
-    Returns (n_bin+1) equi-numbered bin edges of values. They define n_bin
+    Return (n_bin+1) equi-numbered bin edges. These define n_bin
     bins, each of which contains an equal number of points of values.
 
     Parameters
@@ -67,7 +67,7 @@ def equi_num_bins(values, n_bin):
 
     Returns
     -------
-    numpy.array :
+    numpy.array
         equi-numbered bin array
 
     """
@@ -94,6 +94,7 @@ def transform_nan(value):
     -------
     float
         output value
+
     """
     large = 1e30
 
@@ -105,117 +106,10 @@ def transform_nan(value):
     return res
 
 
-class vosError(Exception):
-    """VOS Error.
-
-    Generic error that is raised by the vosHandler.
-
-    """
-
-    pass
-
-
-class vosHandler:
-    """VOS Handler.
-
-    This class manages the use of VOS commands.
-
-    Parameters
-    ----------
-    command : str
-        VOS command name
-
-    """
-
-    def __init__(self, command):
-
-        self._check_vos_install()
-        self._avail_commands = tuple(vosc.__all__)
-        self.command = command
-
-    @staticmethod
-    def _check_vos_install():
-        """Check VOS Install.
-
-        Check if VOS is correctly installed.
-
-        """
-        if import_fail:
-            raise ImportError(
-                'vos package not found, install with \'pip install vos\''
-            )
-
-    @property
-    def command(self):
-        """Command.
-
-        This method sets the VOS command property.
-
-        """
-        return self._command
-
-    @command.setter
-    def command(self, value):
-
-        if value not in self._avail_commands:
-            raise ValueError('vos command must be one of {}'
-                             ''.format(self._avail_commands))
-
-        self._command = getattr(vosc, value)
-
-    def __call__(self, *args, **kwargs):
-        """Call Method.
-
-        This method allows class instances to be called as functions.
-
-        """
-        try:
-            self._command()
-
-        except Exception:
-            raise vosError(f'Error in VOs command: {self._command.__name__}')
-
-
-def download(source, target, verbose=False):
-    """Download.
-
-    Download file from vos.
-
-    Parameters
-    ----------
-    source : str
-        source path on vos
-    target : str
-        target path
-    verbose : bool, optional, default=False
-        verbose output if True
-
-    Returns
-    -------
-    bool
-        status, True/False or success/failure
-
-    """
-    cmd = 'vcp'
-
-    if not os.path.exists(target):
-        sys.argv = [cmd, source, target]
-        if verbose:
-            print('Downloading file {} to {}...'.format(source, target))
-        vcp = vosHandler(cmd)
-
-        vcp()
-        if verbose:
-            print('Download finished.')
-    else:
-        if verbose:
-            print('Target file {} exists, skipping download.'.format(target))
-
-
 def compute_bins_func_2d(x, y, n_bin, mix, weights=None):
     """Compute Bins Func 2D.
 
-    Compute bins in x, y, err, for 2D model
+    Compute bins in x, y, err, for 2D model.
 
     Parameters
     ----------
@@ -232,11 +126,11 @@ def compute_bins_func_2d(x, y, n_bin, mix, weights=None):
 
     Returns
     -------
-    numpy.ndarray(float, 2, n_bin)
+    numpy.ndarray
         bin centers in x_1, x_2
-    numpy.ndarray(float, 2, 2, n_bin)
+    numpy.ndarray
         binned values of y_1, y_2 corresponding to x_1, x_2 bins
-    numpy.ndarray(float, 2, 2, n_bin)
+    numpy.ndarray
         binned errors of y_1, y_2 corresponding to x_1, x_2 bins
 
     """
@@ -320,9 +214,9 @@ def func_bias_2d_full(params, x1, x2, order='lin', mix=False):
     ----------
     params : lmfit.Parameters
         fit parameters
-    x1 : list of float
+    x1 : list
         first component of x-values
-    x2 : list of float
+    x2 : list
         second component of x-values
     order : str, optional
         order of fit, default is 'lin'
@@ -331,9 +225,9 @@ def func_bias_2d_full(params, x1, x2, order='lin', mix=False):
 
     Returns
     -------
-    2D np.array of float
+    np.array
         first component the 2D model y1(x1, x2) on the (x1, x2)-grid
-    2D np.array of float
+    np.array
         second component the 2D model, y2(x1, x2) on the (x1, x2)-grid
 
     """
@@ -373,10 +267,10 @@ def func_bias_2d(params, x1_data, x2_data, order='lin', mix=False):
 
     Returns
     -------
-    float or list of float
+    list
         first component the 2D model, y1(x1, x2). Dimension
         is equal to x1_data and x2_data
-    float or list of float
+    list
         second component the 2D model, y2(x1, x2). Dimension
         is equal to x1_data and x2_data
 
