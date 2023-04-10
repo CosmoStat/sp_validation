61bb18c8ab15bf49ce0d771e9309b169f4866532
Martin Kilbinger <martin.kilbinbger@cea.fr>
2023-04-10 17:19:47 +0200

    map IO fixed

--- notebooks/cosmo_val/cosmo_val.py
+++ notebooks/cosmo_val/cosmo_val.py
@@ -546,11 +546,12 @@ print("Compute aperture-mass dispersion")
 map2 = {}
 for ver in versions:
 
-    out_path = f"{cat['paths']['output']}/map2_{ver}.txt"
+    gg = treecorr.GGCorrelation(TreeCorrConfig)
+    
+    out_fname = f"{cat['paths']['output']}/xi_for_map2_{ver}.txt"
     if os.path.exists(out_fname):
         print(f'Skipping Map2, {out_fname} exists')
         gg.read(out_fname)
-
     else:
         print(f'Computing Map2')
 
@@ -567,9 +568,10 @@ for ver in versions:
             dec_units=coord_units,
             npatch=npatch,
         )
-        gg = treecorr.GGCorrelation(TreeCorrConfig)
+        
         gg.process(cat_gal)
-        gg.writeMapSq(out_path, R=R, m2_uform='Schneider')
+        gg.write(out_fname)
+        #gg.writeMapSq(out_fname, R=R, m2_uform='Schneider')
         print(f"done: {ver}")
 
     mapsq, mapsq_im, mxsq, mxsq_im, varmapsq = gg.calculateMapSq(
