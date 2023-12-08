b20cdbcafab87a920890f4f2182a95fccba36a9f
LisaGoh <lisa.goh@cea.fr>
2023-12-08 15:59:40 +0100

    debugging

--- cosmo_inference/pipeline.sh
+++ cosmo_inference/pipeline.sh
@@ -1,8 +1,8 @@
-!/bin/bash
+#!/bin/bash
+read -p 'ROOT: ' root
 read -p 'FITS FILE FOLDER: ' xi_folder
 read -p 'NZ BLIND:' blind
 read -p 'CHAIN FOLDER: ' data
-read -p 'ROOT: ' root
 mkdir -p data/${root}
 
 ################ STEP 1: CALCULATE COVARIANCE MATRICES###############
@@ -12,13 +12,13 @@ mkdir -p data/${root}/covs
 nz_file="data/nz/nz_shapepipe_${blind}.txt"
 
 # write to the ini file, paths to the nz file
-cat > cosmocov_config/cosmocov_${root}.ini <<EOF
+cat <<EOT >> cosmocov_config/cosmocov_${root}.ini
 
+\n
 shear_REDSHIFT_FILE : $nz_file 
 clustering_REDSHIFT_FILE : $nz_file
 outdir : data/$root/covs/
-
-EOF
+EOT
 
 echo -e "Running CosmoCov...\n"
 
@@ -31,8 +31,9 @@ done
 f="data/${root}/covs/cov_${root}"; cat data/${root}/covs/out_cov* > $f; python scripts/cosmocov_process.py $f
 
 echo -e "Covmats written out to data/${root}/covs/cov_${root}\n"
-################## STEP 2: COMBINE##########################################################
+# ################## STEP 2: COMBINE##########################################################
 echo -e "Combining files...\n"
+
 # Specify paths to the xi_plus and xi_minus fits files, and the covmat txt file
 xip_cat="${xi_folder}/xiplus_shapepipe.fits"
 xim_cat="${xi_folder}/ximinus_shapepipe.fits"
@@ -42,12 +43,12 @@ out_file="data/${root}/cosmosis_${root}.fits"
 
 python scripts/cosmosis_fitting.py $xip_cat $xim_cat $covmat $nz_file $out_file
 
-################# STEP 3: RUN COSMOSIS#####################################################
+# ################# STEP 3: RUN COSMOSIS#####################################################
 # Add path specification within cosmosis config file
-sed "/^\[DEFAULT\]/a\FITS_FILE = ${out_file}" cosmosis_config/cosmosis_pipeline_${root}.ini
-sed "/^\[output\]/a\filename = ${data}/samples_${root}.txt" cosmosis_config/cosmosis_pipeline_${root}.ini
-sed "/^\[pipeline\]/a\values = cosmosis_config/values_${root}.ini" cosmosis_config/cosmosis_pipeline_${root}.ini
-sed "/^\[pipeline\]/a\priors = cosmosis_config/priors_${root}.ini" cosmosis_config/cosmosis_pipeline_${root}.ini
+sed -i "/^\[DEFAULT\]/a\FITS_FILE = ${out_file}" cosmosis_config/cosmosis_pipeline_${root}.ini
+sed -i "/^\[output\]/a\filename = ${data}/samples_${root}.txt" cosmosis_config/cosmosis_pipeline_${root}.ini
+sed -i "/^\[pipeline\]/a\values = cosmosis_config/values_${root}.ini" cosmosis_config/cosmosis_pipeline_${root}.ini
+sed -i "/^\[pipeline\]/a\priors = cosmosis_config/priors_${root}.ini" cosmosis_config/cosmosis_pipeline_${root}.ini
 
 echo -e "Running CosmoSIS...\n"
 
