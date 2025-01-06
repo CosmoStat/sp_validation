c9beb0285c49d6ed01c3ec595711726d8cfe2546
martinkilbinger <martin.kilbinger@cea.fr>
2025-01-06 09:36:21 +0000

    notebooks

--- scripts/prepare_patch_for_spval.sh
+++ scripts/prepare_patch_for_spval.sh
@@ -5,20 +5,20 @@ patch=$1
 spdir=$HOME/astro/repositories/github/sp_validation
 
 # Galaxy catalogue
-mv ~/psfex/final_cat_${patch}.hdf5 .
+cp ~/psfex/final_cat_${patch}.hdf5 .
 
 # Parameter file, to avoid read errors for hdf5 file
-ln -s ~/shapepipe/example/cfis/final_cat.param
+ln -sf ~/shapepipe/example/cfis/final_cat.param
 
 # Star catalogue
-ln -s $HOME/psfex/P3/output/run_sp_Ms/merge_starcat_runner/output/full_starcat-0000000.fits
+ln -sf $HOME/psfex/P3/output/run_sp_Ms/merge_starcat_runner/output/full_starcat-0000000.fits
 
 # Parameter file
 cp $spdir/notebooks/params.py .
 
 # nb/python script
-ln -s $spdir/notebooks/validation.py
+ln -sf $spdir/notebooks/validation.py
 
 # Tile number list
-ln -s ~/shapepipe/auxdir/CFIS/tiles_202106/tiles_${patch}.txt 
+ln -sf ~/shapepipe/auxdir/CFIS/tiles_202106/tiles_${patch}.txt
 
