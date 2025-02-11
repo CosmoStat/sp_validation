cf7817411ebcd950b572dd5efdf31b7961739738
martinkilbinger <martin.kilbinger@cea.fr>
2025-02-11 13:19:40 +0000

    added missing parameter

--- notebooks/params.py
+++ notebooks/params.py
@@ -117,7 +117,7 @@ mmap_mode = None
 add_cols = ["FLUX_RADIUS", "FWHM_IMAGE", "FWHM_WORLD", "MAGERR_AUTO", "MAG_WIN", "MAGERR_WIN", "FLUX_AUTO", "FLUXERR_AUTO", "FLUX_APER", "FLUXERR_APER"]
 
 ## Pre-calibration catalogue, including masked objects and mask flags
-add_cols_pre_cal = ["IMAFLAGS_ISO", "FLAGS", "NGMIX_MCAL_FLAGS", "NGMIX_MOM_FAIL", "NGMIX_ELL_PSFo_NOSHEAR", "N_EPOCH", "NGMIX_N_EPOCH"]
+add_cols_pre_cal = ["IMAFLAGS_ISO", "FLAGS", "NGMIX_MCAL_FLAGS", "NGMIX_MOM_FAIL", "N_EPOCH", "NGMIX_N_EPOCH", "NGMIX_ELL_PSFo_NOSHEAR", "NGMIX_ELL_ERR_NOSHEAR"]
 
 ### Set flag columns as integer format
 add_cols_pre_cal_format = {}
