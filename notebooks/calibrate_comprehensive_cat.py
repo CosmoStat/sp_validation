8a217bf9d5a7c10fe5f9b0e35320d2eab8f77123
martinkilbinger <martinkilbinger@cea.fr>
2025-03-03 08:53:46 +0100

    Fixed print bug; removed previous cut_pre dict

--- notebooks/calibrate_comprehensive_cat.py
+++ notebooks/calibrate_comprehensive_cat.py
@@ -7,7 +7,7 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.15.1
 #   kernelspec:
-#     display_name: sp_validation
+#     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
@@ -39,10 +39,6 @@ obj._params["verbose"] = True
 
 dat, dat_ext = obj.read_cat(load_into_memory=False)
 
-print("MKDEBUG: only use first 10k objects")
-dat = dat[:10000]
-dat_ext = dat_ext[:10000]
-
 print(f"Found {len(dat)} (~{util.millify(len(dat))}) objects in catalogue")
 
 
@@ -61,11 +57,11 @@ class mask():
         
     @classmethod    
     def from_list(cls, masks, label="combined"):
-        mask = cls(label, label, kind="combined", value=None)
+        my_mask = cls(label, label, kind="combined", value=None)
 
-        mask._mask = np.logical_and.reduce([m._mask for m in masks])
+        my_mask._mask = np.logical_and.reduce([m._mask for m in masks])
 
-        return mask
+        return my_mask
 
     def apply(self, dat):
         if self._kind == "equal":
@@ -101,17 +97,13 @@ class mask():
         elif self._kind =="greater_equal":
             sign = ">="
         if sign is not None:
-            print(f"{self._col_name} {sign} {value}", file=f_out)
+            print(f"{self._col_name} {sign} {self._value}", file=f_out)
             
         if self._kind == "range":
             print(f"{self._value[0]} <= {self._col_name} <= {self._value[1]}", file=f_out)            
 
 
 # ### Pre-processing ShapePipe flags
-
-# +
-cut_pre = {}
-labels = {}
 
 masks = []
 
@@ -126,33 +118,6 @@ masks.append(my_mask)
 
 my_mask = mask(col_name="overlap", label="tile overlap", kind="equal", value=True, dat=dat)
 masks.append(my_mask)
-
-# +
-# SExtractor flags (see galaxy.py:classification_galaxy_base)
-
-name = "FLAGS"
-good_mask_value = 0
-
-# MKDBEUG TODO: implement values other than 0 as "good"
-
-cut_pre[name] = (dat[name] == good_mask_value)
-labels[name] = "SE FLAGS"
-
-# +
-# Duplicate objects
-
-name = "overlap"
-good_mask_value = True
-cut_pre[name] = (dat[name] == good_mask_value)
-labels[name] = "tile overlap"
-
-# +
-# ShapePipe mask
-
-name = "IMAFLAGS_ISO"
-good_mask_value = 0
-cut_pre[name] = (dat[name] == good_mask_value)
-labels[name] = "SP mask"
 # -
 
 my_mask = mask(col_name="IMAFLAGS_ISO", label="SP mask", kind="equal", value=0, dat=dat)
@@ -165,31 +130,10 @@ my_mask = mask(col_name="N_EPOCH", label=r"$n_{\rm epoch}$", kind="greater_equal
 masks.append(my_mask)
 
 # +
-# Number of epochs
-
-name = "N_EPOCH"
-val_min = 2
-cut_pre[name] = (dat[name] >= val_min)
-
-# MKDEBUG check NGMIX_N_EPOCH
-labels[name] = r"$n_{\rm epoch}$"
-
-# +
 # Magnitude range
 
 my_mask = mask(col_name="mag", label="mag range", kind="range", value=[15, 30], dat=dat)
 masks.append(my_mask)
-
-# +
-# Magnitude range
-
-name = "mag"
-min_max = [15, 30]
-cut_pre[name] = (
-    (dat[name] >= min_max[0])
-    & (dat[name] <= min_max[1])
-)
-labels[name] = "mag range"
 
 # +
 # ngmix flags
@@ -210,34 +154,12 @@ value = -10
 my_mask = mask(col_name=col_name, label=label, kind=kind, value=value, dat=dat)
 masks.append(my_mask)
 
-
-# +
-# ngmix flags
-
-names = ["NGMIX_MCAL_FLAGS", "NGMIX_MOM_FAIL"]
-good_mask_values = [0, 0]
-for name, good_mask_value in zip(names, good_mask_values):
-    cut_pre[name] = (dat[name] == good_mask_value) 
-labels[names[0]] = "ngmix flag"
-labels[names[1]] = "ngmix moments fail"
-    
-name = "NGMIX_ELL_PSFo_NOSHEAR_0"
-bad_mask_value = -10
-cut_pre[name] = (
-    dat[name] != bad_mask_value
-)
-# MKDEBUG TODO: check should be two components, see galaxy.py.
-labels[name] = "bad PSF ell"
 # -
-
-# Combine all masks with &
-cut_pre_combined = np.logical_and.reduce(list(cut_pre.values()))
 
 print(f"Combining {len(masks)} pre-processing masks")
 mask_combined_pre = mask.from_list(masks, label="combined_pre")
 
 # +
-
 # Output some mask statistics
 
 num_obj = dat.shape[0]
@@ -247,53 +169,15 @@ for my_mask in masks:
     my_mask.print_stats(num_obj)
 
 mask_combined_pre.print_stats(num_obj)
-
-# +
-# Output some mask statistics
-
-num_obj = dat.shape[0]
-
-print(f"{'flag':30s} {'label':30s} {'num_ok':>10} {'num_ok[%]':>10}")
-for name in cut_pre:
-    num_ok = sum(cut_pre[name])
-    print(f"{name:30s} {labels[name]:30s} {num_ok:10d} {num_ok/num_obj:10.2%}")
-name = "combined"
-num_ok = sum(cut_pre_combined)
-print(f"{name:30s} {num_ok:10d} {num_ok/num_obj:10.2%}")
-
-# +
-# Number of "galaxies" (cut_common in main_set_up)
-
-cut_common = cut_pre["overlap"] & cut_pre["FLAGS"] & cut_pre["mag"] & cut_pre["IMAFLAGS_ISO"] & cut_pre["N_EPOCH"]
-name = "common"
-num_ok = sum(cut_common)
-print(f"{name:30s} {num_ok:10d} {num_ok/num_obj:10.2%}")
-
-cut_galaxy = cut_common & cut_pre["NGMIX_MCAL_FLAGS"] & cut_pre["NGMIX_ELL_PSFo_NOSHEAR_0"] & cut_pre["NGMIX_MOM_FAIL"]
-name = "galaxy"
-num_ok = sum(cut_galaxy)
-print(f"{name:30s} {num_ok:10d} {num_ok/num_obj:10.2%}")
 # -
 
 # ### Structural masks
-
-cut_struct = {}
 
 # Get all columns of type ``bool'' from the extended catalogue
 column_ext_info = {
     name: "bool" if dat_ext.dtype.fields[name][0] == "int8" else dat_ext.dtype.fields[name][0]
     for name in dat_ext.dtype.names
 }
-
-# +
-good_mask_values = False
-
-for name in column_ext_info:
-    if column_ext_info[name] == "bool":
-        cut_struct[name] = (dat_ext[name] == good_mask_values)
-        labels[name] = name
-    else:
-        print(f"Type for column {name} not bool, skipping")
 
 # +
 # Get all columns of type ``bool'' from the extended catalogue.
@@ -313,32 +197,11 @@ for name in column_ext_info:
 
 # Number of pointings
 name = "npoint3"
-val_min = 3
-cut_struct[name] = (dat_ext[name] >= val_min)
-labels[name] = r"$n_{\rm pointing}$"
-
-# Number of pointings
-name = "npoint3"
 label = r"$n_{\rm pointing}$"
 value = 3
 kind = "greater_equal"
 my_mask = mask(col_name=name, label=label, kind=kind, value=value, dat=dat_ext)
 masks.append(my_mask)
-
-# Combine all masks with &
-cut_struct_combined = np.logical_and.reduce(list(cut_struct.values()))
-
-# +
-print(f"{'flag':30s} {'label':30s} {'n_ok':>10} {'n_ok[%]':>10}")
-for name in cut_struct:
-    num_ok = sum(cut_struct[name])
-    print(f"{name:30s} {name:30s} {num_ok:10d} {num_ok/num_obj:10.2%}")
-
-name = "combined"
-num_ok = sum(cut_struct_combined)
-print(f"{name:30s} {num_ok:10d} {num_ok/num_obj:10.2%}")
-
-# -
 
 n_struct = len(masks) - idx_first_struct
 print(f"Combining {n_struct} structural masks")
@@ -348,16 +211,10 @@ for my_mask in masks[idx_first_struct:]:
     my_mask.print_stats(num_obj)
 mask_combined_struct.print_stats(num_obj)
 
-# # Plots:
-#
-# # mag histogram, footprint, when applying different cuts
-
 print(f"Combining pre-processing and structural masks, {len(masks)} in total")
 mask_combined = mask.from_list([mask_combined_pre, mask_combined_struct], label="combined")
 
 #
-
-cut_combined = cut_pre_combined & cut_struct_combined
 
 # ### Calibration
 
@@ -410,7 +267,7 @@ gal_metacal = metacal(
 # +
 # Get calibrated quantities
 
-g_corr, g_uncorr, w, mask = calibration.get_calibrated_quantities(gal_metacal)
+g_corr, g_uncorr, w, mask_metacal = calibration.get_calibrated_quantities(gal_metacal)
 
 # Additive bias
 c = np.zeros(2)
@@ -431,22 +288,22 @@ for comp in (0, 1):
 # -
 
 name = "after cuts"
-num_ok = len(mask)
+num_ok = len(masks)
 print(f"{name:30s} {num_ok:10d} {num_ok/num_obj:10.2%}")
 
 # +
 # Additional quantities
 R_shear = np.mean(gal_metacal.R_shear, 2)
 
-ra = cat.get_col(dat, "RA", mask_combined._mask,  mask)
-dec = cat.get_col(dat, "Dec", mask_combined._mask,  mask)
-mag = cat.get_col(dat, "mag", mask_combined._mask,  mask)
-snr = cat.get_snr("ngmix", dat, mask_combined._mask,  mask)
+ra = cat.get_col(dat, "RA", mask_combined._mask,  mask_metacal)
+dec = cat.get_col(dat, "Dec", mask_combined._mask,  mask_metacal)
+mag = cat.get_col(dat, "mag", mask_combined._mask,  mask_metacal)
+snr = cat.get_snr("ngmix", dat, mask_combined._mask,  mask_metacal)
 
 add_cols = ["FLUX_RADIUS", "FWHM_IMAGE", "FWHM_WORLD", "MAGERR_AUTO", "MAG_WIN", "MAGERR_WIN", "FLUX_AUTO", "FLUXERR_AUTO", "FLUX_APER", "FLUXERR_APER"]
 add_cols_data = {}    
 for key in add_cols:
-    add_cols_data[key] = dat[key][mask_combined._mask][mask]
+    add_cols_data[key] = dat[key][mask_combined._mask][mask_metacal]
 
 # +
 # Compute DES weights
@@ -458,8 +315,8 @@ cat_gal["R_g11"] = gal_metacal.R11
 cat_gal["R_g12"] = gal_metacal.R12
 cat_gal["R_g21"] = gal_metacal.R21
 cat_gal["R_g22"] = gal_metacal.R22
-cat_gal["NGMIX_T_NOSHEAR"] = dat["NGMIX_T_NOSHEAR"][mask_combined._mask][mask]
-cat_gal["NGMIX_Tpsf_NOSHEAR"] = dat["NGMIX_Tpsf_NOSHEAR"][mask_combined._mask][mask]
+cat_gal["NGMIX_T_NOSHEAR"] = dat["NGMIX_T_NOSHEAR"][mask_combined._mask][mask_metacal]
+cat_gal["NGMIX_Tpsf_NOSHEAR"] = dat["NGMIX_Tpsf_NOSHEAR"][mask_combined._mask][mask_metacal]
 cat_gal["snr"] = snr
 
 name = 'w_des'
@@ -502,44 +359,50 @@ cat.write_shape_catalog(
 with open("masks.txt", "w") as f_out:
     for my_mask in masks:
         my_mask.print_summary(f_out)
+    for my_mask in masks:
+        my_mask.print_summary(f_out)
 
 from scipy import stats
-def correlation_matrix(mask, confidence_level=0.9):
+def correlation_matrix(masks, confidence_level=0.9):
     
-    n_key = len(mask)
+    n_key = len(masks)
     print(n_key)
 
     cm = np.empty((n_key, n_key))
     r_val = np.zeros_like(cm)
     r_cl = np.empty((n_key, n_key, 2))
 
-    for idx, key1 in enumerate(mask):
-        for jdx, key2 in enumerate(mask):
-            res = stats.pearsonr(mask[key1], mask[key2])
+    for idx, mask_idx in enumerate(masks):
+        for jdx, mask_jdx in enumerate(masks):
+            res = stats.pearsonr(mask_idx._mask, mask_jdx._mask)
             r_val[idx][jdx] = res.statistic
             r_cl[idx][jdx] = res.confidence_interval(confidence_level=confidence_level)
             
     return r_val, r_cl
 
 
+#
+
+all_masks = masks[:-3]
+
 # +
 if not obj._params["cmatrices"]:
     print("Skipping cmatric calculations")
     sys.exit(0)
 
-r_val, r_cl = correlation_matrix(cut_pre)
+r_val, r_cl = correlation_matrix(all_masks)
 
 # +
 
-n = len(cut_pre)
-keys = [key for key in cut_pre]
-labs = [labels[key] for key in cut_pre]
+n = len(all_masks)
+keys = [my_mask._label for my_mask in all_masks]
 
 plt.imshow(r_val, cmap='coolwarm', vmin=-1, vmax=1)
-plt.xticks(np.arange(n), labs)
-plt.yticks(np.arange(n), labs)
+plt.xticks(np.arange(n), keys)
+plt.yticks(np.arange(n), keys)
 plt.xticks(rotation=90)
 plt.colorbar()
+plt.savefig("correlation_matrix.png")
 
 # -
 
@@ -568,15 +431,15 @@ def confusion_matrix(prediction, observation):
     return result
 
 
-n_key = len(keys)
+n_key = len(all_masks)
 cms = np.zeros((n_key, n_key, 2, 2))
-for idx, key_1 in enumerate(keys):
-    for jdx, key_2 in enumerate(keys):
+for idx in range(n_key):
+    for jdx in range(n_key):
 
         if idx == jdx: continue
 
-        print(idx, jdx, key_1, key_2)
-        res = confusion_matrix(cut_pre[key_1], cut_pre[key_2])
+        print(idx, jdx)
+        res = confusion_matrix(masks[idx]._mask, masks[jdx]._mask)
         cms[idx][jdx] = res["cmn"]
 
 # +
@@ -584,54 +447,27 @@ import seaborn as sns
 
 fig = plt.figure(figsize=(30, 30))
 axes = np.empty((n_key, n_key), dtype=object)
-for idx, key_1 in enumerate(keys):
-    for jdx, key_2 in enumerate(keys):
+for idx in range(n_key):
+    for jdx in range(n_key):
         if idx == jdx: continue
         axes[idx][jdx] = plt.subplot2grid((n_key, n_key), (idx, jdx), fig=fig)
 
-
-#fig, axes = plt.subplots(nrows=n_key, ncols=n_key, figsize=(20,20))
 matrix_elements = ["True", "False"]
 
-for idx, key_1 in enumerate(keys):
-    for jdx, key_2 in enumerate(keys):
+for idx in range(n_key):
+    for jdx in range(n_key):
 
         if idx == jdx: continue
         
         ax = axes[idx, jdx]
         sns.heatmap(cms[idx][jdx], annot=True, fmt='.2f', xticklabels=matrix_elements, yticklabels=matrix_elements, ax=ax)
-        ax.set_ylabel(labels[key_1])
-        ax.set_xlabel(labels[key_2])
+        ax.set_ylabel(masks[idx]._label)
+        ax.set_xlabel(masks[jdx]._label)
 
 plt.show(block=False)
+plt.savefig("confusion_matrix.png")
 # -
 
 obj.close_hd5()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
