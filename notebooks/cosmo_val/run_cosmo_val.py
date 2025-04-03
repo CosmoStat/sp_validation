ef66d677866ef7a98e1fcad7b33340fa47ec6d93
martinkilbinger <martinkilbinger@cea.fr>
2025-04-03 16:33:18 +0200

    leakage aux

--- notebooks/cosmo_val/run_cosmo_val.py
+++ notebooks/cosmo_val/run_cosmo_val.py
@@ -1,6 +1,10 @@
 # %%
 # %load_ext autoreload
 # %autoreload 2
+
+import matplotlib
+matplotlib.use("agg")
+
 import matplotlib.pyplot as plt
 import numpy as np
 import treecorr
@@ -8,8 +12,8 @@ from sp_validation.cosmo_val import CosmologyValidation
 
 # %%
 cv = CosmologyValidation(
-    versions=["SP_v1.4.2"],
-    data_base_dir="/n17data/mkilbing/astro/data/",
+    versions=["SP_v1.4.2", "SP_v1.4.5"],
+    data_base_dir="/n17data/mkilbing/astro/data",
     npatch=100,
 )
 
@@ -30,13 +34,19 @@ cv.plot_footprints()
 cv.plot_scale_dependent_leakage()
 
 # %%
-#cv.plot_objectwise_leakage()
+cv.plot_objectwise_leakage()
+
+# %%
+cv.plot_objectwise_leakage_aux()
 
 # %%
 cv.plot_ellipticity()
 
 # %%
 cv.plot_separation()
+
+# %%
+cv.calculate_additive_bias()
 
 # %%
 cv.plot_2pcf()
