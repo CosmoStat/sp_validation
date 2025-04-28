585eb8bd9064a67411f6dafe7f52961872e68bdb
Martin Kilbinger <martin.kilbinger@cea.fr>
2025-04-28 14:00:13 +0200

    added script to compute leakage for minimal cat

---
+++ notebooks/leakage_minimal.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.15.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # Leakage of minimal catalogue
+
+from sp_validation import run_joint_cat as sp_joint
+
+
+# Initialize calibration class instance
+obj = sp_joint.CalibrateCat()
