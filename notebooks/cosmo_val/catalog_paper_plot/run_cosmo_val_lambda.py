6d3f5daa09bc08070fd5c9ffeb361bbc9ff14557
martinkilbinger <martinkilbinger@cea.fr>
2025-12-17 20:05:00 +0100

    updated cosmo_val config to new path structure

--- notebooks/cosmo_val/catalog_paper_plot/run_cosmo_val_lambda.py
+++ notebooks/cosmo_val/catalog_paper_plot/run_cosmo_val_lambda.py
@@ -15,16 +15,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 sys.path.insert(0, '/home/mkilbing/astro/repositories/gitlab.euclid-sgs/FDQA/rho_tau_stats')
-from cosmo_val import CosmologyValidation  # noqa: E402
-
-
-def rename_output(output_bases, output_dir, suff, version_string, rand_str):
-
-    for base in output_bases:
-        old_path = f"{output_dir}/plots/{base}{suff}"
-        new_path  = f"{output_dir}/plots/{base}_{version_string}{rand_str}{suff}"
-        print(f"mv {old_path} -> {new_path}")
-        os.rename(old_path, new_path)
+from cosmo_val import CosmologyValidation, rename_output  # noqa: E402
 
 
 # enable inline plotting for interactive sessions
@@ -33,7 +24,7 @@ if ipython is not None:
     ipython.run_line_magic("matplotlib", "inline")
 
 # Different options
-versions_146 = ["SP_v1.4.6", "SP_v1.4.6_leak_corr"]
+versions_146 = ["SP_v1.4.6"] #, "SP_v1.4.6_leak_corr"]
 versions_var = ["SP_v1.4.5", "SP_v1.4.6", "SP_v1.4.7", "SP_v1.4.8"]
 
 # We use this combination of versions
@@ -45,11 +36,12 @@ theta_min = 1.0
 theta_max = 300.0
 plot_range_fac = 1.1
 
+
 # %%
 cv = CosmologyValidation(
     versions=versions,
-    data_base_dir="/n17data/mkilbing/astro/data/",
     catalog_config="./cat_config.yaml",
+    data_base_dir="/",
     output_dir=output_dir,
     theta_min=theta_min,
     theta_max=theta_max,
@@ -66,17 +58,41 @@ cv = CosmologyValidation(
 output_bases = [
 	"gammat_stars_around_galaxies_lin_non_tomographic",
 	"gammat_stars_around_galaxies_log_non_tomographic",
+	"dgammat_stars_around_galaxies_lin_non_tomographic",
+	"dsize_stars_around_galaxies_lin_non_tomographic",
 ]
 suff = ".png"
 version_string = "_".join(versions)
 
-
+# lambda_1
+print("Computing  λ_1...")
 cv.plot_gammat_stars_around_galaxies(offset=0.025, gammax=True, wo_rand_subtr=True)
 cv.plot_gammat_stars_around_galaxies(offset=0.025, gammax=True, logy=True, wo_rand_subtr=True)
+
+# lambda_2
+print("Computing  λ_2...")
+cv.plot_dgammat_stars_around_galaxies(offset=0.025, gammax=True, wo_rand_subtr=True)
+
+# lambda_3
+print("Computing  λ_3...")
+cv.plot_dsize_stars_around_galaxies(offset=0.025, gammax=True, wo_rand_subtr=True)
+
+
 rename_output(output_bases, output_dir, suff, version_string, "_wo_rand_subtr")
 
 # %%
+# lambda_1
+print("Computing  λ_1...")
 cv.plot_gammat_stars_around_galaxies(offset=0.025, gammax=True)
 cv.plot_gammat_stars_around_galaxies(offset=0.025, gammax=True, logy=True)
+
+# lambda_2
+print("Computing  λ_2...")
+cv.plot_dgammat_stars_around_galaxies(offset=0.025, gammax=True)
+
+# lambda_3
+print("Computing  λ_3...")
+cv.plot_dsize_stars_around_galaxies(offset=0.025, gammax=True)
+
 rename_output(output_bases, output_dir, suff, version_string, "")
 
