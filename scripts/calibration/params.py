f8d349e0fe3c679f69e3d8fc2eacb66d7b77c3ba
Cail Daley <cail.daley@cea.fr>
2026-07-20 02:00:16 +0200

    Revert "Merge pull request #288 from CosmoStat/chore/remove-vestigial-pixel-size"
    This reverts commit 016c33141a8c8ed3d5f9ccfc4916e36d6543b6d9, reversing
    changes made to f21e31e5161b6cac064688da50e8e26b23e3c414.

--- scripts/calibration/params.py
+++ scripts/calibration/params.py
@@ -32,6 +32,9 @@ print("Field name = {}".format(name))
 ## Area of a tile in deg^2
 area_tile = 0.25
 
+## Pixel size in arcsec
+pixel_size = 0.187
+
 ## Shape measurement method (only 'ngmix' is supported):
 ##  'ngmix': multi-epoch model fitting
 shape = "ngmix"
